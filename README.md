344db56c91cef1dff3379664dfa1fd5608cdc0fd
Ryan Sonnek <ryan@codecrate.com>
2014-12-24 23:55:41 -0600

    add initial cheatsheets

---
+++ README.md
@@ -0,0 +1,4 @@
+
+Resources:
+* Cheatset gem - https://github.com/Kapeli/cheatset
+* example cheatsheets - https://github.com/Kapeli/cheatsheets/
---
+++ cheatsheets/watch.rb
@@ -0,0 +1,42 @@
+cheatsheet do
+  title 'Watch'
+  docset_file_name 'Watch'
+  keyword 'watch'
+  source_url 'http://github.com/wireframe/cheatsheets'
+
+  category do
+    id 'Usage'
+    entry do
+      notes <<-'END'
+        constantly execute a process and watch the results
+
+        Usage:
+        ```
+        watch [OPTIONS] [COMMAND]
+
+
+        Example:
+
+         ```bash
+         watch sudo /usr/local/sbin/rabbitmqctl list_queues
+         ```
+      END
+    end
+  end
+
+  category do
+    id 'Execution'
+    entry do
+      command '-n'
+      name 'number of seconds between polls'
+      notes <<-'END'
+        Example: inspect rabbitmq queues every 5 seconds
+
+         ```bash
+         watch -n 5 sudo /usr/local/sbin/rabbitmqctl list_queues
+         ```
+      END
+    end
+    end
+  end
+end
